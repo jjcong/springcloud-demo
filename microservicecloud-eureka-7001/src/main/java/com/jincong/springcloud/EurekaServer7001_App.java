c44824394e9b704698b0653dacecfc0a67fdc01d
jjcong <jincong00@163.com>
2019-11-03 17:22:34 +0800

    SpringBoot 备注

--- microservicecloud-eureka-7001/src/main/java/com/jincong/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/jincong/springcloud/EurekaServer7001_App.java
@@ -3,7 +3,13 @@ package com.jincong.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * Eureka Server
+ * 
+ * @author  j_cong
+ * @date    2019/11/03
+ * @version V1.0
+ */
 @SpringBootApplication
 @EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
 public class EurekaServer7001_App {
