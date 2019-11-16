ed8d0093ee2ee0adf2582dac7ec0bf850c454065
jjcong <jincong00@163.com>
2019-11-16 23:56:35 +0800

    添加zuul模块

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/jincong/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,21 @@
+package com.jincong.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+/**
+ * Zuul主服务
+ * 
+ * @author  j_cong
+ * @date    2019/11/16
+ * @version V1.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+
+}
